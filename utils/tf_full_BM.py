fba384adc9d3b59eb6b174b22eebdcc15f3186aa
Luke Iwanski <luke@codeplay.com>
2017-10-11 17:59:30 +0100

    dont group legend

--- utils/tf_full_BM.py
+++ utils/tf_full_BM.py
@@ -298,7 +298,7 @@ class Workspace(object):
         file_exists = os.path.exists(self.webpage)
         if not file_exists:
             f = open(self.webpage, "w")
-            f.write("<link href='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.min.css' rel='stylesheet'><script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js' charset='utf-8'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.js'></script><div id='chart'></div><script>var chart = c3.generate({data:{x:'date',xFormat:'%Y-%m-%d',url:'"+self.report+"',type:'line'},bindto:'#chart',axis:{x:{type:'timeseries',tick:{format:'%Y-%m-%d'}}}});</script>")
+            f.write("<link href='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.min.css' rel='stylesheet'><script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js' charset='utf-8'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.js'></script><div id='chart'></div><script>var chart = c3.generate({data:{x:'date',xFormat:'%Y-%m-%d',url:'"+self.report+"',type:'line'},tooltip:{grouped:false},bindto:'#chart',axis:{x:{type:'timeseries',tick:{format:'%Y-%m-%d'}}}});</script>")
             f.close()
 
 def main():
