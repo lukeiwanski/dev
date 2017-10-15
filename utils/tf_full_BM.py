b395ebcd59d4b58643ed04800934b4795a24dcc3
Luke Iwanski <luke@codeplay.com>
2017-10-15 23:24:52 +0100

    Use hashes to track performance regression

--- utils/tf_full_BM.py
+++ utils/tf_full_BM.py
@@ -32,6 +32,7 @@ class Repo(object):
         self.BRANCH = branch
         self.DIRNAME = url.split("/")[-1].split(".")[0]
         self.WORKSPACE = workspace
+        self.HASH = branch
 
     def pull_git(self):
         cmd = "cd " + self.WORKSPACE
@@ -42,24 +43,21 @@ class Repo(object):
         os.system(cmd)
         print(bcolors.OKBLUE + "DONE!" + bcolors.ENDC)
 
-    def pull_hg(self):
-        cmd = "cd " + self.WORKSPACE
-        if not os.path.exists(self.WORKSPACE + "/" + self.DIRNAME):
-            cmd += " && hg clone " + self.URL
-        cmd += " && cd " + self.DIRNAME + " && hg up " + self.BRANCH + " && hg pull"
-        print(bcolors.WARNING + cmd + bcolors.ENDC)
-        os.system(cmd)
-        print(bcolors.OKBLUE + "DONE!" + bcolors.ENDC)
+        proc = subprocess.Popen("cd " + self.WORKSPACE + "/" + self.DIRNAME + " && git rev-parse --verify HEAD", shell = True, stdout=subprocess.PIPE)
+        (out, err) = proc.communicate()
+        self.HASH = out.rstrip()
+
+    def hash(self):
+        return self.HASH
 
 class Workspace(object):
-    def __init__(self, computecpp_root, workspace, eigen_branch, tf_branch,
-                 dlbench_branch, benchmarks_branch, package, report, tf_build_options,
-                 webpage):
+    def __init__(self, computecpp_root, workspace, tf_branch, benchmarks_branch,
+                 package, report, tf_build_options, webpage):
         self.tf_build_options = tf_build_options
         self.now = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
         self.report = report
         self.workspace = workspace
-        self.webpage =webpage
+        self.webpage = webpage
         self.mkdir_and_go(self.workspace)
         self.tmp = self.mkdir("tmp")
         self.log = self.mkdir("log")
@@ -73,16 +71,24 @@ class Workspace(object):
         self.tensorflow = Repo("https://github.com/lukeiwanski/tensorflow.git", tf_branch, self.tmp)
         self.benchmarks = Repo("https://github.com/tensorflow/benchmarks.git",
                                benchmarks_branch, self.tmp)
-        self.dlbench = Repo("https://github.com/tfboyd/dlbench.git",
-                            dlbench_branch, self.tmp)
-        self.eigen = Repo("https://bitbucket.org/mehdi_goli/opencl", eigen_branch, self.tmp)
         self.ip = socket.gethostbyname(socket.gethostname())
+        self.tf_branch = tf_branch
+
+    def gen_version(self, str_, rep_):
+        ret = str_.split(") (")
+        clang = ret[0].split(" ")[-1]
+        llvm = ret[1].split(" ")[-1]
+        return rep_[:7] + "-" + clang[:7]+"-"+llvm[:7]
+
 
     def setup(self):
         self.tensorflow.pull_git()
         self.benchmarks.pull_git()
-        self.dlbench.pull_git()
-        self.eigen.pull_hg()
+        cwd = self.tmp
+        my_env = os.environ
+        compiler = self.computecpp + "/bin/compute++" + " --version"
+        self.workspace_version = self.gen_version(str_ = self.execute(cmd = compiler , log_modifier = "compiler_version_gen", cwd = cwd, my_env = my_env)[1], rep_ = self.tensorflow.hash())
+
 
     def mkdir(self, path, delete = False):
         if os.path.exists(path):
@@ -94,6 +100,44 @@ class Workspace(object):
         else:
             os.mkdir(path)
         return self.workspace + "/" + path
+
+    def mkdir_and_go(self, path):
+        self.mkdir(path)
+        os.chdir(path)
+
+    def execute(self, cmd, log_modifier, cwd = "", my_env = []):
+        print(bcolors.WARNING + cmd + bcolors.ENDC)
+
+        fail_log_file_path = self.log+"/"+"FAIL-" + log_modifier + ".log"
+        fail_log_file = open(fail_log_file_path, "w")
+        fail_log_file.write('exec ${PAGER:-/usr/bin/less} "$0" || exit 1 \n')
+        fail_log_file.close()
+        fail_log_file = open(fail_log_file_path, "a+")
+
+        pass_log_file_path = self.log+"/"+"PASS-" + log_modifier + ".log"
+        pass_log_file = open(pass_log_file_path, "w")
+        pass_log_file.write('exec ${PAGER:-/usr/bin/less} "$0" || exit 1 \n')
+        pass_log_file.close()
+        pass_log_file = open(pass_log_file_path, "a+")
+
+        p = subprocess.Popen(cmd, env=my_env, cwd=cwd, shell=True,
+                             stdout=pass_log_file, stderr=fail_log_file,
+                             stdin=subprocess.PIPE)
+        p.communicate()
+        fail_log_file.close()
+        os.chmod(fail_log_file_path, 0755)
+        pass_log_file.close()
+        os.chmod(pass_log_file_path, 0755)
+
+        with open(pass_log_file_path, 'r') as content_file:
+            content = content_file.read()
+
+        if p.returncode == 0:
+            print(bcolors.OKBLUE + "PASS: " + bcolors.ENDC + log_modifier + " " + pass_log_file_path)
+        else:
+            print(bcolors.FAIL + "FAIL: " + bcolors.ENDC + log_modifier + " " + fail_log_file_path)
+
+        return p.returncode, content
 
     def fetch(self, url, workspace, directory_, file_path, delete = False):
         print("Fetching "+ url)
@@ -138,52 +182,6 @@ class Workspace(object):
         print(bcolors.OKBLUE + "DONE!" + bcolors.ENDC)
         return path
 
-
-    def mkdir_and_go(self, path):
-        self.mkdir(path)
-        os.chdir(path)
-
-    def execute(self, cmd, log_modifier, cwd = "", my_env = []):
-        print(bcolors.WARNING + cmd + bcolors.ENDC)
-
-        fail_log_file_path = self.log+"/"+"FAIL-" + log_modifier + ".log"
-        fail_log_file = open(fail_log_file_path, "w")
-        fail_log_file.write('exec ${PAGER:-/usr/bin/less} "$0" || exit 1 \n')
-        fail_log_file.close()
-        fail_log_file = open(fail_log_file_path, "a+")
-
-        pass_log_file_path = self.log+"/"+"PASS-" + log_modifier + ".log"
-        pass_log_file = open(pass_log_file_path, "w")
-        pass_log_file.write('exec ${PAGER:-/usr/bin/less} "$0" || exit 1 \n')
-        pass_log_file.close()
-        pass_log_file = open(pass_log_file_path, "a+")
-
-        p = subprocess.Popen(cmd, env=my_env, cwd=cwd, shell=True,
-                             stdout=pass_log_file, stderr=fail_log_file,
-                             stdin=subprocess.PIPE)
-        p.communicate()
-        fail_log_file.close()
-        os.chmod(fail_log_file_path, 0755)
-        pass_log_file.close()
-        os.chmod(pass_log_file_path, 0755)
-
-        if p.returncode == 0:
-            print(bcolors.OKBLUE + "PASS: " + bcolors.ENDC + log_modifier + " " + pass_log_file_path)
-        else:
-            print(bcolors.FAIL + "FAIL: " + bcolors.ENDC + log_modifier + " " + fail_log_file_path)
-
-        return p.returncode
-
-    def build_bench_eigen(self):
-        print("Compiling and Running Eigen Benchmarks")
-        cwd = self.tmp + "/" + self.eigen.DIRNAME+"/bench/tensors"
-        my_env = os.environ
-        my_env["COMPUTECPP_PACKAGE_ROOT_DIR"] = self.computecpp
-
-        cmd = "bash eigen_sycl_bench.sh"
-        self.execute(cmd=cmd, log_modifier=self.ip+"eigen", cwd=cwd, my_env=my_env)
-        print(bcolors.OKBLUE + "DONE!" + bcolors.ENDC)
-
     def build_install_tf(self):
         print("Compiling and Installing TF")
         cwd = self.tmp + "/" + self.tensorflow.DIRNAME
@@ -226,7 +224,7 @@ class Workspace(object):
         my_env["LD_LIBRARY_PATH"] = self.computecpp+"/lib"
         my_env["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-        models = ["alexnet", "vgg16", "resnet50", "inception3"]
+        models = ["trivial", "alexnet", "vgg16", "resnet50", "inception3"]
         # using gpu device is more flexible here - node that suppose to be
         # accelerated on GPU will use SYCL device when aviable
         targets = ["cpu", "gpu"]
@@ -242,20 +240,6 @@ class Workspace(object):
             cmd = "python tf_cnn_benchmarks.py --batch_size=32 --num_batches=10 --device="+target+" --model="+model+" --data_format=NHWC --trace_file="+self.now+"_"+file_name+".js"
             self.execute(cmd=cmd, log_modifier=file_name, cwd=cwd, my_env=my_env)
 
-    def run_dlbench(self):
-        print("Running dlbench")
-        cwd = self.tmp + "/" + self.dlbench.DIRNAME+"/tools/tensorflow/fc"
-        my_env = os.environ
-        my_env["LD_LIBRARY_PATH"] = self.computecpp+"/lib"
-        my_env["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
-        cmd = "python fcn5_mnist.py --use_dataset=False --epochs=1 --log_device_placement=True --data_dir="+ cwd + "/mnist_datasets"
-        self.execute(cmd=cmd, log_modifier=self.ip+"_dlbench_gpu", cwd=cwd, my_env=my_env)
-
-        cmd = "python fcn5_mnist.py --use_dataset=False --epochs=1 --log_device_placement=True --device_id=-1 --data_dir="+ cwd + "/mnist_datasets"
-        self.execute(cmd=cmd, log_modifier=self.ip+"_dlbench_cpu", cwd=cwd, my_env=my_env)
-
-
     def gen_csv_based_on_log(self):
         file_name = self.report
         file_exists = os.path.exists(file_name)
@@ -266,9 +250,9 @@ class Workspace(object):
 
         with open(file_name, append_write) as f:
             writer = csv.writer(f)
-            header = ["date"]
+            header = ["version"]
             path = os.path.join(self.log, "*PASS*.log")
-            row = [self.now]
+            row = [self.workspace_version]
             for filename in glob.glob(path):
                 with open(filename) as f:
                     col_name = ""
@@ -299,8 +283,8 @@ class Workspace(object):
                 writer.writerows([row])
 
     def gen_webpage(self):
-        filename = "report.csv"
-        code = "<link href='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.min.css' rel='stylesheet'><script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js' charset='utf-8'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.js'></script><div id='chart'></div><script>var chart = c3.generate({data:{x:'date',xFormat:'%Y-%m-%d',url:'"+self.report+"',type:'scatter'},tooltip:{grouped:false},bindto:'#chart',axis:{y:{show:true,max:100,min:0,ticks:5,padding:{top:1,bottom:0},},x:{type:'timeseries',tick:{format:'%Y-%m-%d'}}}});</script>"
+        filename = self.report
+        code = "<link href='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.min.css' rel='stylesheet'><script src='https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.17/d3.js' charset='utf-8'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.18/c3.js'></script><div id='chart'></div><script>var chart = c3.generate({data:{x:'version',url:'"+self.report+"',type:'scatter'},tooltip:{grouped:false},bindto:'#chart',axis:{y:{show:true,max:100,min:0,ticks:5,padding:{top:1,bottom:0},},x:{type:'category'}}});</script>"
 
         # date
         a = [np.genfromtxt(filename, usecols=[0], delimiter=',', dtype=np.unicode_)]
@@ -308,22 +292,20 @@ class Workspace(object):
         # sorted columns
         b = sorted(np.transpose(np.genfromtxt(filename, delimiter=',', dtype=np.unicode_))[1:], key=lambda tup: tup[0])
         sort = b[:]
-        #
 
         for x in range((len(b)) / 2 ):
             m1_ = sort[2*x][1:]
             max1_ = np.amax(np.asarray(m1_, dtype=np.float32)) * 2
 
             m2_ = sort[2*x+1][1:]
-            max2_ = np.amax(np.asarray(m2_, dtype=np.float32)) * 2
+            max2_ = np.amax(np.asarray(m1_, dtype=np.float32)) * 2
 
-            if max2_ > max1_:
-                max1_ = max2_
+            max1_ = np.maximum(max1_, max2_)
 
             out = np.transpose(np.concatenate(([a[0]], [b[2*x]], [b[2*x+1]]), axis=0))
             df = pd.DataFrame(out)
             df.to_csv(str(x) + ".csv", header=None, index=False)
-            code +="<div id='c"+str(x)+"'></div><script>var chart = c3.generate({data:{x:'date',xFormat:'%Y-%m-%d',url:'"+str(x)+".csv',type:'line'},tooltip:{grouped:false},bindto:'#c"+str(x)+"',axis:{y: { show: true, max:"+str(max1_)+", min:0, ticks : 5,padding: {top:1, bottom:0},}, x:{type:'timeseries',tick:{format:'%Y-%m-%d'}}}});</script>"
+            code +="<div id='c"+str(x)+"'></div><script>var chart = c3.generate({data:{x:'version',url:'"+str(x)+".csv',type:'line'},tooltip:{grouped:false},bindto:'#c"+str(x)+"',axis:{y: { show: true, max:"+str(max1_)+", min:0, ticks : 5,padding: {top:1, bottom:0},}, x:{type:'category'}}});</script>"
 
         file_exists = os.path.exists(self.webpage)
         if not file_exists:
@@ -339,14 +321,10 @@ def main():
                         help="Version of ComputeCpp to download")
     parser.add_argument("-w", "--workspace", dest="workspace", default=os.getenv("HOME")+"/autogen_workspace",
                         help="Where to create workspace")
-    parser.add_argument("-e", "--eigen_branch", dest="eigen", default="Eigen-OpenCL-Optimised",
-                        help="Eigen branch to use")
     parser.add_argument("-t", "--tf_branch", dest="tf", default="dev/amd_gpu",
                         help="TensorFlow branch to use")
     parser.add_argument("-b", "--benchmarks_branch", dest="benchmarks", default="master",
                         help="TensorFlow Benchmarks branch to use")
-    parser.add_argument("-d", "--blbench_branch", dest="dlbench", default="data_sets",
-                        help="DLBench branch to use")
     parser.add_argument("-r", "--csv", dest="report", default="report.csv",
                         help="CSV report file name")
     parser.add_argument("-l", "--html", dest="webpage", default="index.html",
@@ -357,16 +335,16 @@ def main():
     args = parser.parse_args()
 
     workspace = Workspace(computecpp_root=args.computecpp,
-                          workspace=args.workspace, eigen_branch=args.eigen,
-                          tf_branch=args.tf, benchmarks_branch=args.benchmarks,
-                          dlbench_branch=args.dlbench, package=args.package,
-                          report=args.report, tf_build_options=args.tf_build_options,
+                          workspace=args.workspace,
+                          tf_branch=args.tf,
+                          benchmarks_branch=args.benchmarks,
+                          package=args.package,
+                          report=args.report,
+                          tf_build_options=args.tf_build_options,
                           webpage=args.webpage)
     workspace.setup()
-#    workspace.build_bench_eigen()
     workspace.build_install_tf()
     workspace.run_benchmarks()
-#    workspace.run_dlbench()
     workspace.gen_csv_based_on_log()
     workspace.gen_webpage()
 
